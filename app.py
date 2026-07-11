a2f2a94bddcf04bbf0a2b963430527d5ca90c8bf
Hope Tadsen <hope.tadsen@ischool.berkeley.edu>
2026-07-11 14:11:24 -0700

    updated tab button

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def Olympics():
     file='image.png'
     return render_template('Olympics.html',file=file)
 
+@app.route('/tab')
+def tab():  # Changed from 'embed' to 'tab'
+    return render_template('test_tableau.html')
+
 if __name__ == '__main__':
     app.run()
