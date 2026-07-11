9aa8aa295594a6acd6eabf31f1846840fe68f6b4
Hope Tadsen <hope.tadsen@ischool.berkeley.edu>
2026-07-11 13:29:38 -0700

    testing new file name and image correction

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def w209():
+def Olympics():
     file='about9.jpg'
-    return render_template('w209.html',file=file)
+    return render_template('Olympics.html',file=file)
 
 if __name__ == '__main__':
     app.run()
