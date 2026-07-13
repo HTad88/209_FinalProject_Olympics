92ce69d70b4a3232c0a5ff4d1efd2372a8345edf
bjulve-ischool <153471309+bjulve-ischool@users.noreply.github.com>
2026-07-13 02:53:22 -0700

    Configure app.py to serve the static html within the site folder.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(
-    __name__
+    __name__,
+    static_folder='209_OlympicsHiddenCosts/site',
+    static_url_path='/'
 )
 
-@app.route('/')
+@app.route('/Olympics')
 def Olympics():
     file='image.png'
     return render_template('Olympics.html',file=file)
@@ -12,6 +14,15 @@ def Olympics():
 @app.route('/tab')
 def tab():  # Changed from 'embed' to 'tab'
     return render_template('test_tableau.html')
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+# Dynamically routes paths like /dashboard.html or /help.html
+@app.route('/<path:url_path>')
+def serve_static_page(url_path):
+    return app.send_static_file(url_path)
 
 if __name__ == '__main__':
     app.run()
